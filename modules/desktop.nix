cfaff294d8f1a3dd4ecae905cdff5f56f61d5fce
Karl Rivest Harnois <karlrivestharnois@gmail.com>
2026-07-12 20:44:01 -0400

    Automatically start hyprland

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -3,6 +3,17 @@
 {
   programs.hyprland.enable = true;
 
+  services.greetd = {
+    enable = true;
+    settings = {
+      initial_session = {
+        command = "Hyprland";
+        user = "karl";
+      };
+      default_session.command = "${pkgs.tuigreet}/bin/tuigreet --cmd Hyprland";
+    };
+  };
+
   environment.systemPackages = with pkgs; [
     kitty
   ];
