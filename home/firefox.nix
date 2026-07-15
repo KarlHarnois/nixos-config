9027ff1ae927c6f87f6a910fbb9d52754f4564f6
Karl Rivest Harnois <karlrivestharnois@gmail.com>
2026-07-14 22:27:12 -0400

    Force dark chrome and content in personal firefox
    The activeThemeID pref alone does not activate the dark theme on
    current firefox, so set browser.theme.toolbar-theme and content-theme
    to dark and make web content prefer dark color schemes.

--- home/firefox.nix
+++ home/firefox.nix
@@ -1,5 +1,20 @@
 { ... }:
 
+let
+  theme = import ./theme.nix;
+
+  themedChrome = ''
+    :root {
+      --lwt-accent-color: #${theme.background} !important;
+      --toolbar-bgcolor: #${theme.background} !important;
+      --tab-selected-bgcolor: #${theme.surface} !important;
+      --toolbar-field-background-color: #${theme.surface} !important;
+      --toolbar-field-focus-background-color: #${theme.surface} !important;
+      --toolbar-field-focus-border-color: #${theme.accent} !important;
+      --arrowpanel-background: #${theme.surface} !important;
+    }
+  '';
+in
 {
   programs.firefox = {
     enable = true;
@@ -8,6 +23,16 @@
       personal = {
         id = 0;
         isDefault = true;
+
+        settings = {
+          "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
+          "browser.theme.toolbar-theme" = 0;
+          "browser.theme.content-theme" = 0;
+          "layout.css.prefers-color-scheme.content-override" = 0;
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        };
+
+        userChrome = themedChrome;
       };
 
       work = {
