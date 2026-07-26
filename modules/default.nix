b80839139acc7ede93b356a148ef8ddd9f37b1ac
Karl Rivest Harnois <karlrivestharnois@gmail.com>
2026-07-25 22:29:22 -0400

    Add a generic L2TP over IPsec client
    Runs charon and xl2tpd, pointing both at /var/lib/l2tp for the connection
    definition and credentials. Nothing about a particular VPN lives here, and
    neither unit starts until those files are present.
    
    services.strongswan cannot be used: it renders connections into the store,
    and services.xl2tpd only speaks LNS.

--- modules/default.nix
+++ modules/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./core.nix
     ./desktop.nix
+    ./l2tp-client.nix
     ./voxtype.nix
   ];
 }
---
+++ modules/l2tp-client.nix
@@ -0,0 +1,61 @@
+{ pkgs, ... }:
+
+let
+  stateDir = "/var/lib/l2tp";
+
+  # Declaring connections here would put the gateway address and the PSK in the
+  # world-readable Nix store.
+  daemonConf = pkgs.writeText "strongswan.conf" ''
+    charon {
+      plugins {
+        stroke {
+          secrets_file = ${stateDir}/ipsec.secrets
+        }
+      }
+    }
+
+    starter {
+      config_file = ${stateDir}/ipsec.conf
+    }
+  '';
+in
+{
+  boot.kernelModules = [ "l2tp_ppp" ];
+
+  environment.systemPackages = [ pkgs.strongswan ];
+
+  systemd.tmpfiles.rules = [ "d ${stateDir} 0700 root root -" ];
+
+  systemd.services = {
+    strongswan = {
+      description = "strongSwan IPsec daemon";
+      wantedBy = [ "multi-user.target" ];
+      wants = [ "network-online.target" ];
+      after = [ "network-online.target" ];
+      path = [
+        pkgs.iproute2
+        pkgs.iptables
+        pkgs.kmod
+        pkgs.util-linux
+      ];
+      environment.STRONGSWAN_CONF = daemonConf;
+      unitConfig.ConditionPathExists = "${stateDir}/ipsec.conf";
+      serviceConfig.ExecStart = "${pkgs.strongswan}/sbin/ipsec start --nofork";
+    };
+
+    xl2tpd = {
+      description = "xl2tpd L2TP client";
+      wantedBy = [ "multi-user.target" ];
+      wants = [ "network-online.target" ];
+      after = [
+        "network-online.target"
+        "strongswan.service"
+      ];
+      unitConfig.ConditionPathExists = "${stateDir}/xl2tpd.conf";
+      serviceConfig = {
+        ExecStart = "${pkgs.xl2tpd}/bin/xl2tpd -D -c ${stateDir}/xl2tpd.conf -C /run/xl2tpd/l2tp-control";
+        RuntimeDirectory = "xl2tpd";
+      };
+    };
+  };
+}
