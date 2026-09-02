26cf427adb11a5ebf0fb3ee3441afed0b4d2f1ac
Karl Rivest Harnois <karlrivestharnois@gmail.com>
2026-09-01 20:57:58 -0400

    Stop exporting the mtgo command as a flake package
    The package existed so the command also ran on Arch. MTGO now runs in the
    Windows VM, whose command is bound to its host and has no such export.

--- flake.nix
+++ flake.nix
@@ -112,7 +112,6 @@
         vm = vmRunner;
         vmToplevel = vmSystem.config.virtualisation.vmVariant.system.build.toplevel;
         l2tp-commands = pkgs.callPackage ./modules/l2tp-client/commands.nix { };
-        mtgo = pkgs.callPackage ./home/mtgo/command.nix { };
       };
 
       formatter.${system} = pkgs.nixfmt-tree;
