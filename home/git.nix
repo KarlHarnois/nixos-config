13b53b81b507216087ab0b9d2a535b2b767c5fbe
Karl Rivest Harnois <karlrivestharnois@gmail.com>
2026-09-09 00:29:10 -0400

    Consolidate home-manager programs into one attribute set

--- home/git.nix
+++ home/git.nix
@@ -1,34 +1,36 @@
 {
-  programs.git = {
-    enable = true;
+  programs = {
+    git = {
+      enable = true;
 
-    settings = {
-      user = {
-        name = "Karl Rivest Harnois";
-        email = "karlrivestharnois@gmail.com";
+      settings = {
+        user = {
+          name = "Karl Rivest Harnois";
+          email = "karlrivestharnois@gmail.com";
+        };
+
+        core.editor = "nvim";
+
+        alias = {
+          w = "worktree";
+          wa = "worktree add";
+          wr = "worktree remove";
+          aa = "add --all";
+          b = "branch";
+          c = "commit";
+          co = "checkout";
+          ds = "diff --staged";
+          fp = "push origin HEAD --force-with-lease";
+          p = "push origin HEAD";
+          s = "status";
+        };
+
+        rebase.autosquash = true;
       };
-
-      core.editor = "nvim";
-
-      alias = {
-        w = "worktree";
-        wa = "worktree add";
-        wr = "worktree remove";
-        aa = "add --all";
-        b = "branch";
-        c = "commit";
-        co = "checkout";
-        ds = "diff --staged";
-        fp = "push origin HEAD --force-with-lease";
-        p = "push origin HEAD";
-        s = "status";
-      };
-
-      rebase.autosquash = true;
     };
+
+    gh.enable = true;
+
+    lazygit.enable = true;
   };
-
-  programs.gh.enable = true;
-
-  programs.lazygit.enable = true;
 }
